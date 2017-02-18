81d7d3c3d1c7fec39b313ef21c556822c6a046bd
Jon <>
2017-02-18 14:21:03 -0700

    decription of classes

--- CodeDayApp/src/codedayapp/Menus.java
+++ CodeDayApp/src/codedayapp/Menus.java
@@ -10,5 +10,7 @@ package codedayapp;
  * @author jonbo
  */
 public class Menus {
+    //this calss will call each class for each separate category and help the user navigate the app
+    //this class will use a gui and will be called in the main class
     
 }
